e02349c32a8a342b9ad5668a6d71e9599dfdec85
ibetacat <sametime@foxmail.com>
2018-03-19 23:09:12 +0800

    test

---
+++ ms-project/ms-user/src/main/java/com/ms/config/UserConfig.java
@@ -0,0 +1,40 @@
+package com.ms.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+@Component
+@RefreshScope
+@ConfigurationProperties(prefix = "user")
+public class UserConfig {
+
+	private String name;
+	private String age;
+	private String sex;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAge() {
+		return age;
+	}
+
+	public void setAge(String age) {
+		this.age = age;
+	}
+
+	public String getSex() {
+		return sex;
+	}
+
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+
+}
--- ms-project/ms-user/src/main/java/com/ms/user/UserTest.java
+++ ms-project/ms-user/src/main/java/com/ms/user/UserTest.java
@@ -1,19 +1,21 @@
 package com.ms.user;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ms.config.UserConfig;
 
 @RestController
 @RequestMapping("/user")
 public class UserTest {
-	
-	@Value("dev-test2")
-	String config;
-	
+
+	@Autowired
+	UserConfig userConfig;
+
 	@RequestMapping("/config")
 	public String getConfigInfo() {
-		return config;
+		return "user-1 " + userConfig.getName() + " " + userConfig.getAge() + " " + userConfig.getSex();
 	}
 
 }
